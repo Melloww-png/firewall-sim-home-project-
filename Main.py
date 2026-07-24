53c9dfcdddb2539c926c2ed9e358ed29b45e0054
Daniel CH <danielclementrich09@gmail.com>
2026-07-25 00:26:12 +0800

    Initial commit

---
+++ Main.py
@@ -0,0 +1,16 @@
+# Fake network packet
+packet = {
+    "ip": "192.168.1.10",
+    "port": 80,
+    "protocol": "TCP"
+}
+
+# Firewall rule
+blocked_ip = "192.168.1.10"
+
+# Check the packet
+if packet["ip"] == blocked_ip:
+    print("❌ NOT ALLOWED/Blocked")
+    print("Reason: IP address is blocked.")
+else:
+    print("✅ ALLOWED")
