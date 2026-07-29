1cc36865dc4b9c573f64e633cb81e3510019af95
Daniel CH <danielclementrich09@gmail.com>
2026-07-30 00:33:14 +0800

    added protocol filtering and user inputs

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 # Fake network packet
 packet = {
-    "ip": "192.168.1.20",
-    "port": 80,
-    "protocol": "TCP"
+    "ip": input("Enter IP Address: "),
+    "port": int(input("Enter Port: ")),
+    "protocol": input("Enter Protocol: ").upper()
 }
 
 # Firewall rules
 blocked_ips = ["192.168.1.10", "10.0.0.5"]
 blocked_ports = [23, 21]
+blocked_protocols = ["UDP"]
 
-# Check the packet
+# Checking the packet
 if packet["ip"] in blocked_ips:
     print("❌ BLOCKED")
     print("Reason: IP address is blocked.")
@@ -17,6 +18,10 @@ if packet["ip"] in blocked_ips:
 elif packet["port"] in blocked_ports:
     print("❌ BLOCKED")
     print("Reason: Port is blocked.")
+
+elif packet["protocol"] in blocked_protocols:
+    print("❌ BLOCKED")
+    print("Reason: Protocol is blocked.")   
 
 else:
     print("✅ ALLOWED")
